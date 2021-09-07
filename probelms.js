8d685a7ccd6a0df1ee8e7c4b7ecb65da8430dcca
Dhakshin601ARIVOLI <85745912+Dhakshin601ARIVOLI@users.noreply.github.com>
2021-09-07 19:18:32 +0530

    Add files via upload

---
+++ probelms.js
@@ -0,0 +1,96 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ground;
+
+var top_wall;
+var ball;
+var ball1;
+var con1;
+var con;
+var btn2;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+   
+   var ball_options = {
+    restitution: 0.95,
+  }
+  var ball1_options = {
+    restitution:0.95,
+  }
+   btn2= createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(hForce);
+  
+   
+  
+  
+
+  ground =new Ground(200,390,400,20);
+
+
+  ball = Bodies.circle(100,200,20,ball_options);
+  World.add(world,ball);
+  con=Matter.Constraint.create({ 
+    pointA:{x:200,y:20},
+    bodyB:ball,pointB:{x:0,y:0},length:100,stiffness:0.1
+  });
+
+  ball1 = Bodies.circle(100,200,20,ball1_options);
+  World.add(world,ball);
+  con1=Matter.Constraint.create({ 
+    pointA:{x:200,y:20},
+    bodyB:ball,pointB:{x:0,y:0},length:100,stiffness:0.1
+  });
+
+
+
+  
+  World.add(world,con)
+  World.add(world,con1)
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+
+  ellipse(ball.position.x,ball.position.y,20);
+  ellipse(ball1.position.x,ball1.position.y,20);
+  ground.show();
+  strokeWeight(2);
+  stroke(256);
+  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
+  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
+  Engine.update(engine);
+}
+
+
+function hForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:-0});
+}
+function hForce1()
+{
+  Matter.Body.applyForce(ball1,{x:0,y:0},{x:0.05,y:-0});
+}
+  
+
+
